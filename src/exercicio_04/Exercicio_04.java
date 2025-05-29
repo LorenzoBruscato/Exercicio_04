a20a6194e9e6219a9f42158e433bb36595479277
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-05-28 21:00:23 -0300

    Exercício_04

---
+++ src/exercicio_04/Exercicio_04.java
@@ -0,0 +1,20 @@
+package exercicio_04;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class Exercicio_04 {
+
+    public static void main(String[] args) {
+        String nome;
+
+        nome = "João";
+        JOptionPane.showMessageDialog(null, nome);
+
+        nome = "Maria";
+        JOptionPane.showMessageDialog(null, "O valor da variável é: " + nome);
+    }
+}
